02225333d2f15b44b02c4909b2aa417e05ffa5c4
Jonathon Wyza <iamwyza@gmail.com>
2022-12-01 16:12:46 -0600

    minor cleanup

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,45 +1,41 @@
 ﻿var lines = await File.ReadAllLinesAsync("input.txt");
 
 // part1
+int runningTotal = 0;
+int max = 0;
+
+foreach (var line in lines)
 {
-    int runningTotal = 0;
-    int max = 0;
-
-    foreach (var line in lines)
+    if (string.IsNullOrEmpty(line))
     {
-        if (string.IsNullOrEmpty(line))
+        if (runningTotal > max)
         {
-            if (runningTotal > max)
-            {
-                max = runningTotal;
-            }
-
-            runningTotal = 0;
-            continue;
+            max = runningTotal;
         }
 
-        runningTotal += int.Parse(line);
+        runningTotal = 0;
+        continue;
     }
 
-    Console.WriteLine($"Part 1: {max}");
+    runningTotal += int.Parse(line);
 }
+
+Console.WriteLine($"Part 1: {max}");
 
 // Part 2
+Dictionary<int, int> totals = new() { { 0, 0 } };
+int elfIndex = 0;
+foreach (var line in lines)
 {
-    Dictionary<int, int> totals = new (){{0,0}};
-    int elfIndex = 0;
-    foreach (var line in lines)
+    if (string.IsNullOrEmpty(line))
     {
-        if (string.IsNullOrEmpty(line))
-        {
-            elfIndex++;
-            totals.Add(elfIndex, 0);
-            continue;
-        }
-
-        totals[elfIndex] += int.Parse(line);
+        elfIndex++;
+        totals.Add(elfIndex, 0);
+        continue;
     }
 
-    int top3 = totals.OrderByDescending(x => x.Value).Take(3).Sum(y => y.Value);
-    Console.WriteLine($"Part 2: {top3}");
+    totals[elfIndex] += int.Parse(line);
 }
+
+int top3 = totals.OrderByDescending(x => x.Value).Take(3).Sum(y => y.Value);
+Console.WriteLine($"Part 2: {top3}");
