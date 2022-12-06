856cbc5f39a7d2a02ca08f9a2a0482eb480345c8
Jonathon Wyza <iamwyza@gmail.com>
2022-12-06 17:45:08 -0600

    Day6

---
+++ AdventOfCode2022/Day6/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022.Day6;
+internal class Solution : DayBase
+{
+    private static int FindStart(string input, int length)
+    {
+        for (int i = length; i < input.Length; i++)
+        {
+            if (input[(i - length)..i].Distinct().Count() == length)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    
+
+    public override async Task RunPart1()
+    {
+        PrintStart(1);
+        var lines = await GetLines();
+
+        var start = FindStart(lines[0], 4);
+       
+        Console.WriteLine($"Packet starts at {start}");
+    }
+
+    public override async Task RunPart2()
+    {
+        PrintStart(2);
+
+        var lines = await GetLines();
+
+        var start = FindStart(lines[0], 14);
+
+        Console.WriteLine($"Packet starts at {start}");
+
+
+    }
+}
